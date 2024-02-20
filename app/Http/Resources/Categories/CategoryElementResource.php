54ccc299d988214d5168c7f5b8af848a086fbdbe
Tamer Jood <tamerjood32@gmail.com>
2024-02-21 00:26:28 +0300

    first init1

--- app/Http/Resources/Categories/CategoryElementResource.php
+++ app/Http/Resources/Categories/CategoryElementResource.php
@@ -17,7 +17,9 @@ class CategoryElementResource extends JsonResource
         return [
             'id' => $this->id,
             'title_en' => $this->title_en,
-            'title_ar' => $this->title_ar,
+            // 'title_ar' => $this->title_ar,
+            'is_add_on' => $this->is_add_on,
+            'add_on_title_en' => $this->add_on_title_en,
         ];
     }
 }
