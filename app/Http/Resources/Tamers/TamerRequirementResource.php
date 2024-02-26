621d3f65d64a61535f8e64984082d912d2ad45c3
Tamer Jood <tamerjood32@gmail.com>
2024-02-26 15:48:38 +0300

    Edit Tamer Resource

--- app/Http/Resources/Tamers/TamerRequirementResource.php
+++ app/Http/Resources/Tamers/TamerRequirementResource.php
@@ -15,13 +15,18 @@ class TamerRequirementResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id'=>$this->id,
-            'requirement_text'=>$this->requirement_text,
-            'answer_type'=>$this->answer_type,
-            'is_mandatory_requirement'=>$this->is_mandatory_requirement,
-            'is_allow_more_than_one_answer_for_multi_choice'=>$this->is_allow_more_than_one_answer_for_multi_choice,
-            'for_multiple_choices'=>TamerRequirementForMultipleChoiceResource::collection($this->forMultipleChoices),
-            
+            'id' => $this->id,
+            'requirement_text' => $this->requirement_text,
+            'answer_type' => $this->answer_type,
+            'is_mandatory_requirement' => $this->is_mandatory_requirement,
+            'is_allow_more_than_one_answer_for_multi_choice' => $this->is_allow_more_than_one_answer_for_multi_choice,
+            'firstـchoice_text' => $this->firstـchoice_text,
+            'secondـchoice_text' => $this->secondـchoice_text,
+            'thirdـchoice_text' => $this->thirdـchoice_text,
+            'fourthـchoice_text' => $this->fourthـchoice_text,
+            'fifthـchoice_text' => $this->fifthـchoice_text,
+            'sixthـchoice_text' => $this->sixthـchoice_text,
+
         ];
     }
 }
